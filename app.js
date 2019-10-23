888980efbdf154782e1e8cfc14b1193e07faa859
unn.krigul <unn.krigul@khk.ee>
2019-10-23 16:17:46 +0300

    Andmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('JS test2');
+// Andmetüübid
+
+//Täisarvud
+
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// Ujuvkomaarv
+
+console.log(2.5);
+console.log(3.14);
+console.log(-7.23);
+console.log(2.0);
+
+// Sõned (string)
+
+console.log('Hello World')
+
+// Tõeväärtused
+
+console.log(true); // tõene
+console.log(false); // väär
+
+// Nimekirjad (array)
+
+console.log ([1, 2, 3, 4]);
+
+// Objektid
+
+console.table({a : 1, b : 2})

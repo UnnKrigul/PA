468075f700d63ab5f1a7e78f660e167cc0a82d4a
unn.krigul <unn.krigul@khk.ee>
2019-10-28 11:37:27 +0200

    FOR filtering loop mis ei tööta

--- app.js
+++ app.js
@@ -1,115 +1,18 @@
-// If tingimuslause
-// Tingimuslause tingimuslause sees
-
-let pin = 1234;
-let kontoJaak = 100;
-let valja = 50;
-
-if (pin == 1234) {
-    console.log ("parool õige");
-    console.log ("Teie konto jääk on " + kontoJaak + "€");
-    console.log ("Soovite välja võtta " + valja + "€")
-
-    if (valja <= kontoJaak) {
-        console.log ("Palun võtke oma raha");
-        kontoJaak = (kontoJaak - valja);
-        console.log ("Teie konto jääk on " + kontoJaak + "€")
+let numbrid =[]
+for(let kord = 0; kord < numbrid.length; kord++){
+    for(let i = 0; i < 10; i++) {
+        let arv = Math.floor(Math.random() * 10);
+        numbrid.push (arv);
+        console.log (numbrid[i])
     }
 
-    else {
-        console.log ("Kontol pole piisavalt raha")
+    for(let i = 0; i < numbrid.length; i++) {
+        let abi;
+        if (numbrid[i] > numbrid[i+1]){
+            abi = numbrid[i];
+            numbrid[i] = numbrid[i+1];
+            numbrid[i+1] = abi
+        }
     }
 }
-
-else {
-    console.log ("parool vale");
-}
-
-// Mitmeosalised tingimused
-// AND
-
-let pin1 = 1;
-let pin2 = 2;
-
-if (pin1 == 1 && pin2 == 2) {
-    console.log ("Seif avaneb");
-}
-
-else {
-    console.log ("Politsei viib päti vangi");
-}
-
-// OR
-
-if (pin1 == 1 || pin2 == 2) {
-    console.log ("Seif avaneb");
-}
-
-else {
-    console.log ("Politsei viib päti vangi");
-}
-
-// NOT
-
-let value = 1;
-
-if (value !== 1) {
-    console.log (true);
-}
-
-else {
-    console.log (false);
-}
-
-// Mitmeharulised if laused elif-iga
-
-let punktid = 95;
-
-if (punktid > 90) {
-    console.log ("hinne A");
-}
-    else if (punktid > 80) {
-        console.log ("hinne B");
-    }
-    else if (punktid > 70) {
-        console.log ("hinne C");
-    }
-    else if (punktid > 60) {
-        console.log("hinne D");
-    }
-    else if (punktid > 50) {
-        console.log ("hinne E");
-    }
-    else {
-        console.log ("Töö tuleb uuesti teha")
-    }
-
-// Tsüklid/Loops
-// For tsükkel 
-
-let arv;
-let lause;
-
-for (arv = 1; arv <= 10; arv++) {
-    lause = 'Arv = ';
-    if (arv < 10) {
-        lause = lause + ' ';
-    }
-    lause = lause + arv;
-    if (arv % 2 == 0) {
-        lause = lause + ' - paaris';
-    }
-    else {
-    lause = lause + ' - paaritu'
-    }
-    console.log (lause)
-    if (arv >= 5) {
-        break;
-    }
-}
-
-let autod =['Kia', 'Toyota', 'Ford', 'Nissan']
-
-for(let i = 0; i < autod.length; i++) {
-    console.log(autod[i])
-}
+console.log(numbrid)

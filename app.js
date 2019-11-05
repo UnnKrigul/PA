d481e6b64fad2cbd3806e2e60210da47900a92a5
unn.krigul <unn.krigul@khk.ee>
2019-11-05 15:18:49 +0200

    paarsus, väljastus eraldi funktsioonidega

--- app.js
+++ app.js
@@ -8,20 +8,40 @@
 // funktsiooni defineerimine
 function paarsusK(arv){
     // defineerime suvalise täisarvu
+    let lause;
+    // arvutame jäägi 2-ga jagamisel
+    let jaak = arv % 2;
+    
 
-// arvutame jäägi 2-ga jagamisel
-let jaak = arv % 2;
-
-//kui jääk on võrnde 0ga
-
-if( jaak === 0){
-    console.log(arv + ' on paaris')
-}
-else {
-    console.log(arv + ' ei ole paaris')
-}
+    //kui jääk on võrnde 0ga
+    if( jaak === 0){
+        lause = arv + ' on paaris';
+    }  else {
+        lause = arv + ' ei ole paaris';
+    }
+    return lause;
 }
 
 // funktsiooni kasutamine
-paarsusK(2);
+let paarsus = paarsusK(3);
+console.log(paarsus);
+
+
+
+function ristkylikuPindala(pikkus, laius){
+    let pindala = pikkus * laius;
+    return pindala;
+}
+
+
+function v2ljastus(pikkus, laius){
+    
+    console.log('Pikkus = ' + pikkus);
+    console.log('Laius = ' + laius);
+    let rPindala = ('Pindala = ' + ristkylikuPindala(2, 4));
+    console.log(rPindala);
+}
+
+v2ljastus(2, 4)
+
 

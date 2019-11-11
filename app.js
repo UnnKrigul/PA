7fc0b567336604b05a8bdf284186e4f285259f2a
unn.krigul <unn.krigul@khk.ee>
2019-11-11 10:34:04 +0200

    Scope 2

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // Global
 
-let a = 1;
+var a = 1;
 let b = 2;
 const c = 3;
 
-console.log('Global', a, b, c);
+console.log('Global ', a, b, c);
+
+// block
+
+if (true){
+    var a = 4;
+    let b = 5;
+    const c = 6;
+    console.log('Block ', a, b, c);
+}
+console.log('Global ', a, b, c);
+
+// function 
+
+function testScope(){
+    var a = 7;
+    let b = 8;
+    const c = 9;
+    console.log('Function ', a, b, c);
+}
+testScope();
+console.log('Globaalne ', a, b, c)
